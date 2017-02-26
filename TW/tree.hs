1aeab1dec72bcf2861247475641d503ab7b42cd3
Cheatinikle <kunwoo3526@gmail.com>
2017-02-26 18:24:52 +0900

    Added getFromTree Function

--- TW/tree.hs
+++ TW/tree.hs
@@ -2,6 +2,18 @@ module TW.Tree where
 
 import Data.Typeable
 data Tree a = Empty | Node a [Tree a]
+  deriving (Eq, Show)
 
 treeType :: (Typeable a) => Tree a -> TypeRep
 treeType (Node a _) = typeOf a
+
+getFromTree :: (Typeable a, Eq a) => Int -> Tree a -> Tree a
+getFromTree n tree = treeList [tree] !! (n-1) where
+  treeList [] = []
+  treeList ts = ts ++ (treeList (concatMap childsOfNode ts))
+
+valOfNode :: (Typeable a) => Tree a -> a
+valOfNode (Node x _) = x
+
+childsOfNode :: (Eq a) => Tree a -> [Tree a]
+childsOfNode (Node _ xs) = filter (\x -> x /= Empty) xs
